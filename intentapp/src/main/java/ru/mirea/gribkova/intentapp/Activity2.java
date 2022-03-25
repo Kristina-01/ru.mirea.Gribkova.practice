dd9e635a8505a5397d0d10c8e7b08b5925d5edfc
Kristina-01 <gribkova01@bk.ru>
2022-03-25 20:38:57 +0300

    Initial commit

---
+++ intentapp/src/main/java/ru/mirea/gribkova/intentapp/Activity2.java
@@ -0,0 +1,33 @@
+package ru.mirea.gribkova.intentapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Activity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        long dateInMillis = System.currentTimeMillis();
+        String format = "yyyy-MM-dd HH:mm:ss";
+        final SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String dateString = sdf.format(new Date(dateInMillis));
+
+        Intent intent = getIntent();
+        String string = intent.getStringExtra("message");
+
+    }
+
+
+}
---
+++ intentapp/src/main/java/ru/mirea/gribkova/intentapp/MainActivity.java
@@ -0,0 +1,30 @@
+package ru.mirea.gribkova.intentapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String string = intent.getStringExtra("message");
+
+        tv = (TextView) findViewById(R.id.textView1);
+        //tv.setText(string);
+    }
+
+
+}
